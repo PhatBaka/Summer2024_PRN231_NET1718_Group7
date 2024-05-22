2d6d0b87dda577ceadd49801d86e2d94829a2b2e
PhatBaka <nguyenquyphat2711@gmail.com>
2024-05-22 16:43:55 +0700

    feat: page UI

---
+++ Front-end/JewelryStoreUI/Pages/Accounts/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JewelryStoreUI.Pages.Accounts
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Front-end/JewelryStoreUI/Pages/Orders/Detail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JewelryStoreUI.Pages.Orders
+{
+    public class DetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
