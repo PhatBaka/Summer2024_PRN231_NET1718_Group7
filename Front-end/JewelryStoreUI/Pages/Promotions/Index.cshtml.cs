a58d2b322c38cdf915040d7afd18f3a94f73860d
HoangNH2812 <114154942+HoangNH2812@users.noreply.github.com>
2024-05-31 00:33:23 +0700

    index, create, detail promotion
    base code, not css, just test on index page only

--- Front-end/JewelryStoreUI/Pages/Promotions/Index.cshtml.cs
+++ Front-end/JewelryStoreUI/Pages/Promotions/Index.cshtml.cs
@@ -1,12 +1,40 @@
+using JewelryShop.DTO.DTOs;
+using JewelryStoreUI.Pages.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace JewelryStoreUI.Pages.Promotions
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public List<StoreDiscountDTO> StoreDiscounts { get; set; }
+        [BindProperty]
+        public ResponseResult<dynamic> ResponseResult { get; set; }
+
+        private string url = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("API_URL").Value;
+
+        private void LoadData()
         {
+            url += "StoreDiscount";
+            ResponseResult = new ResponseResult<dynamic>();
+        }
+
+        public async Task<PageResult> OnGetAsync()
+        {
+            LoadData();
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
+
+                dynamic responseResult = JsonConvert.DeserializeObject(result);
+                ResponseResult.Data = JsonConvert.SerializeObject(responseResult);
+                StoreDiscounts = JsonConvert.DeserializeObject<List<StoreDiscountDTO>>(ResponseResult.Data);
+            }
+            return Page();
         }
     }
 }
