6eb81e8f209bde45bd7bc3a483df2621625aa04b
PhatBaka <nguyenquyphat2711@gmail.com>
2024-06-01 14:00:20 +0700

    fix: on-progress load orderDetail

--- Front-end/JewelryStoreUI/Pages/Orders/Detail.cshtml.cs
+++ Front-end/JewelryStoreUI/Pages/Orders/Detail.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace JewelryStoreUI.Pages.Orders
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public dynamic Order { get; set; }
+        [BindProperty]
+        public IList<dynamic> Orders { get; set; }
+        private string url = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("API_URL").Value;
+        private string orderUrl;
+
+        public DetailModel()
         {
+            orderUrl += $"{url}Order";
+            Orders = new List<dynamic>();
+        }
+
+        public async Task OnGetAsync(string orderId)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(orderUrl);
+                var result = await response.Content.ReadAsStringAsync();
+                Orders = JsonConvert.DeserializeObject<IList<dynamic>>(result);
+                Order = Orders.FirstOrDefault(o => o.orderId == orderId);
+            }
         }
     }
 }
--- Front-end/JewelryStoreUI/Pages/Orders/Index.cshtml.cs
+++ Front-end/JewelryStoreUI/Pages/Orders/Index.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace JewelryStoreUI.Pages.Orders
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public IList<dynamic> Orders { get; set; }
+        private string url = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("API_URL").Value;
+        private string orderUrl;
+
+        public IndexModel()
         {
+            orderUrl += $"{url}Order";
+            Orders = new List<dynamic>();
+        }
+
+        public async Task OnGetAsync()
+        {
+            using (var client  = new HttpClient()) 
+            {
+                var response = await client.GetAsync(orderUrl);
+                var result = await response.Content.ReadAsStringAsync();
+                Orders = JsonConvert.DeserializeObject<IList<dynamic>>(result);
+            }
         }
     }
 }
