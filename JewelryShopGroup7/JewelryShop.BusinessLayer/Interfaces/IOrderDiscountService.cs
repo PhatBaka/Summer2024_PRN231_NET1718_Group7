859ed75034accc88fe42916e093c6ad1408202c5
PhatBaka <nguyenquyphat2711@gmail.com>
2024-06-15 13:54:27 +0700

    fix: discount interface

--- JewelryShopGroup7/JewelryShop.BusinessLayer/Interfaces/IOrderDiscountService.cs
+++ JewelryShopGroup7/JewelryShop.BusinessLayer/Interfaces/IOrderDiscountService.cs
@@ -5,5 +5,8 @@ namespace JewelryShop.BusinessLayer.Interfaces
     public interface IOrderDiscountService : IService<OrderDiscountDTO>
     {
         public Task<Guid> UpdateDiscount(string? tiername ,string? OrderDiscountCode, decimal? offerPercent, decimal? total);
-    }
+		public Task<decimal?> UpdateTotalPriceaddOff(decimal offerPercent, decimal? total);
+		public Task<decimal?> UpdateTotalPriceaddStoreCode(string? Code, decimal? total);
+		public Task<decimal?> UpdateTotalPriceaddTier(string? tier, decimal? total);
+	}
 }
