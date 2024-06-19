a056d6396c4a8d679a0a6e07130f615215f26ac1
HoangNH2812 <114154942+HoangNH2812@users.noreply.github.com>
2024-06-19 16:23:11 +0700

    fix IOrderDiscountService  after merger

--- JewelryShopGroup7/JewelryShop.BusinessLayer/Interfaces/IOrderDiscountService.cs
+++ JewelryShopGroup7/JewelryShop.BusinessLayer/Interfaces/IOrderDiscountService.cs
@@ -6,5 +6,8 @@ namespace JewelryShop.BusinessLayer.Interfaces
     public interface IOrderDiscountService : IService<OrderDiscountResponse,CreateOrderDiscountRequest,UpdateOrderDiscountRequest,OrderDiscountFilter>
     {
         public Task<Guid> UpdateDiscount(string? tiername ,string? OrderDiscountCode, decimal? offerPercent, decimal? total);
+        Task<decimal?> updateTotalPriceaddOff(decimal offerPercent, decimal? total);
+        Task<decimal?> updateTotalPriceaddStoreCode(string? Code, decimal? total);
+        Task<decimal?> updateTotalPriceaddTier(string? tier, decimal? total);
     }
 }
