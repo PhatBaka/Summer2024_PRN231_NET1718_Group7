3eb09966ef9aa38a8d03ab04b4f61a16f1dddac7
caothang7a7 <caothang7a7@gmail.com>
2024-06-21 13:17:36 +0700

    add iservice for update price by add dis

--- JewelryShopGroup7/JewelryShop.BusinessLayer/Interfaces/IOrderDiscountService.cs
+++ JewelryShopGroup7/JewelryShop.BusinessLayer/Interfaces/IOrderDiscountService.cs
@@ -5,5 +5,8 @@ namespace JewelryShop.BusinessLayer.Interfaces
     public interface IOrderDiscountService : IService<OrderDiscountDTO>
     {
         public Task<Guid> UpdateDiscount(string? tiername ,string? OrderDiscountCode, decimal? offerPercent, decimal? total);
+        public Task<decimal?> updateTotalPriceaddOff(decimal offerPercent, decimal? total);
+        public Task<decimal?> updateTotalPriceaddStoreCode(string? Code, decimal? total);
+        public Task<decimal?> updateTotalPriceaddTier(string? tier, decimal? total);
     }
 }
